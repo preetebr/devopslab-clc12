328266446ed41c3bd50133b1208dab72b4b66f14
Kelvyn <52673971+preetebr@users.noreply.github.com>
2024-12-22 18:40:55 +0000

    Update

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask
+from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
+csrf = CSRFProtect(app)
 
 @app.route("/")
 def pagina_inicial():
-    return "Kelvyn v1.0"
+    return "Kelvyn v2.0"
